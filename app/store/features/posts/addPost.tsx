e019089b79ca2ab97930eba05cf453ce0e9dee9b
rajan <rajannagpal99@gmail.com>
2024-02-27 19:18:13 +0530

    update code

--- app/store/features/posts/addPost.tsx
+++ app/store/features/posts/addPost.tsx
@@ -1,15 +1,15 @@
 import { View, TextInput, Text, Button } from "react-native"
-import react, { useState ,useId} from 'react';
+import react, { useState, useId } from 'react';
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useDispatch, useSelector } from "react-redux";
 import { addNewPosts, postsActins } from "./posts.slice";
 import { v4 as uuidv4 } from 'uuid';
-import {Picker} from '@react-native-picker/picker';
+import { Picker } from '@react-native-picker/picker';
 
 type props = NativeStackScreenProps<any>;
 
 export const AddPost: react.FC<props> = ({ route, navigation }) => {
-const users = useSelector((d:any)=>d.users);
+    const users = useSelector((d: any) => d.users);
 
     const dispatch = useDispatch<any>();
     const [title, setTitle] = useState<string>('');
@@ -17,10 +17,16 @@ const users = useSelector((d:any)=>d.users);
     const [userId, setUserId] = useState<string>('');
 
     const submit = () => {
-        if(title && content){
-        // dispatch(postsActins.postAdded({ id:uuidv4().substring(0,10).replaceAll("-",""), title: title, content: content ,userId}))
-        dispatch(addNewPosts({title:title,content:content,userId:userId}));
-        navigation.pop();
+        if (title && content) {
+            // dispatch(postsActins.postAdded({ id:uuidv4().substring(0,10).replaceAll("-",""), title: title, content: content ,userId}))
+            try {
+                dispatch(addNewPosts({ title: title, content: content, userId: userId }));
+            } catch (err) {
+                console.log('Failed to save the post', err);
+            } finally {
+
+            }
+            navigation.pop();
         }
     }
     return <View style={{ padding: 10, }}>
@@ -36,8 +42,8 @@ const users = useSelector((d:any)=>d.users);
                 <TextInput style={{ height: 30, padding: 10 }} onChangeText={setContent} placeholder="Please enter Content" />
             </View>
         </View>
-        <Picker selectedValue={userId} onValueChange={(item,index)=>setUserId(item)}>
-            {users.map((d:any)=><Picker.Item label={d.name} value={d.id}/>)}
+        <Picker selectedValue={userId} onValueChange={(item, index) => setUserId(item)}>
+            {users.map((d: any) => <Picker.Item label={d.name} value={d.id} />)}
         </Picker>
         <View style={{ paddingVertical: 10 }}>
             <Button title="Submit" onPress={submit} />
